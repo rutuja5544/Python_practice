bd7cba4db0d1353d613ea18ed3068edc78984a66
rutuja5544 <48381858+rutuja5544@users.noreply.github.com>
2019-04-08 18:11:32 +0530

    Add files via upload

---
+++ gamguessing.py
@@ -0,0 +1,17 @@
+import random				
+a=int(input("Enter No:"))
+
+r=random.randint(1,11)
+
+for i in range(3):
+	if r==a:
+		i+
+		print("guess is correct")
+
+	elif r<a:
+		i+
+		print("guess no is greater")
+	
+	elif r>a:
+		i+
+		print("guess no is lower")
